d19daf7ace0b0c04ff9e85492bf73f9fb5a54d00
Daniel Pittman <dpdanpittman@gmail.com>
2019-04-15 20:15:26 -0700

    initial commit

---
+++ src/AppTopbar.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class AppTopbar extends Component {
+
+    static defaultProps = {
+        onToggleMenu: null
+    }
+
+    static propTypes = {
+        onToggleMenu: PropTypes.func.isRequired
+    }
+
+    render() {
+        return (
+            <div className="layout-topbar clearfix">
+                <div className="layout-topbar-text">
+                    <button className="p-link">
+                        <span className="layout-topbar-item-text">Events</span>
+                        <span className="layout-topbar-icon pi pi-calendar"/>
+                    </button>
+                    <button className="p-link">
+                        <span className="layout-topbar-item-text">Settings</span>
+                        <span className="layout-topbar-icon pi pi-cog"/>
+                    </button>
+                    <button className="p-link">
+                        <span className="layout-topbar-item-text">User</span>
+                        <span className="layout-topbar-icon pi pi-user"/>
+                    </button>
+					<button className="p-link">
+                        <span className="layout-topbar-item-text">Events</span>
+                        <span className="layout-topbar-icon pi pi-calendar"/>
+                    </button>
+                    <button className="p-link">
+                        <span className="layout-topbar-item-text">Settings</span>
+                        <span className="layout-topbar-icon pi pi-cog"/>
+                    </button>
+                    <button className="p-link">
+                        <span className="layout-topbar-item-text">User</span>
+                        <span className="layout-topbar-icon pi pi-user"/>
+                    </button>
+					<button className="p-link layout-menu-button" onClick={this.props.onToggleMenu}>
+                    <span className="pi pi-bars"/>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
